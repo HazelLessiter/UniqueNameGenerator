3bb8c49c2f3f58116fb78f60a887502b22e45055
HazelLessiter <hazellessiter0@gmail.com>
2018-06-05 16:11:47 +0100

    Created random name generator.

---
+++ UniqueNameGenerator/Controllers/PrefixesController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UniqueNameGenerator.Models;
+
+namespace UniqueNameGenerator.Controllers
+{
+    public class PrefixesController : Controller
+    {
+        private DBEntities db = new DBEntities();
+
+        // GET: Prefixes
+        public ActionResult Index()
+        {
+            Random rnd = new Random();
+            int i = 0;
+            List<string> names = new List<string>();//Create list that contains names
+            for (i = 0; i < 22; i++)//For Loop 22 times
+            {
+                int maxPrefixID = db.Prefixes.Max(u => u.PrefixID);//Get max prefix ID in database
+                int randomPrefixID = rnd.Next(maxPrefixID);//Get random number from 0 to max ID in prefix database
+                string prefix = db.Prefixes.Where(x => x.PrefixID == randomPrefixID).Select(x => x.Prefix1).First();//Grab random prefix from database
+                int maxSuffixID = db.Suffixes.Max(a => a.SuffixID);//Get max suffix ID in database
+                int randomSuffixID = rnd.Next(maxSuffixID);//Get random number from 0 to max ID in suffix database
+                string suffix = db.Suffixes.Where(x => x.SuffixID == randomSuffixID).Select(x => x.Suffix1).First();//Grab random suffix from database
+                names.Add(prefix+suffix);//Add string = prefix + suffix to list
+            }
+            //Send list to view 
+            return View(names.ToList());
+        }
+
+        // GET: Prefixes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prefix prefix = db.Prefixes.Find(id);
+            if (prefix == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prefix);
+        }
+
+        // GET: Prefixes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Prefixes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PrefixID,Prefix1")] Prefix prefix)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Prefixes.Add(prefix);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(prefix);
+        }
+
+        // GET: Prefixes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prefix prefix = db.Prefixes.Find(id);
+            if (prefix == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prefix);
+        }
+
+        // POST: Prefixes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PrefixID,Prefix1")] Prefix prefix)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(prefix).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(prefix);
+        }
+
+        // GET: Prefixes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prefix prefix = db.Prefixes.Find(id);
+            if (prefix == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prefix);
+        }
+
+        // POST: Prefixes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Prefix prefix = db.Prefixes.Find(id);
+            db.Prefixes.Remove(prefix);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ UniqueNameGenerator/Controllers/SuffixesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UniqueNameGenerator.Models;
+
+namespace UniqueNameGenerator.Controllers
+{
+    public class SuffixesController : Controller
+    {
+        private DBEntities db = new DBEntities();
+
+        // GET: Suffixes
+        public ActionResult Index()
+        {
+            return View(db.Suffixes.ToList());
+        }
+
+        // GET: Suffixes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suffix suffix = db.Suffixes.Find(id);
+            if (suffix == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suffix);
+        }
+
+        // GET: Suffixes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Suffixes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SuffixID,Suffix1")] Suffix suffix)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Suffixes.Add(suffix);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(suffix);
+        }
+
+        // GET: Suffixes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suffix suffix = db.Suffixes.Find(id);
+            if (suffix == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suffix);
+        }
+
+        // POST: Suffixes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SuffixID,Suffix1")] Suffix suffix)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(suffix).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(suffix);
+        }
+
+        // GET: Suffixes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Suffix suffix = db.Suffixes.Find(id);
+            if (suffix == null)
+            {
+                return HttpNotFound();
+            }
+            return View(suffix);
+        }
+
+        // POST: Suffixes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Suffix suffix = db.Suffixes.Find(id);
+            db.Suffixes.Remove(suffix);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
